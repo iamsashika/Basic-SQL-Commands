71b516a841367e45d8c24335e93c9e773441c4dd
Sashika Chandrasena <78399078+iamsashika@users.noreply.github.com>
2022-12-10 12:35:56 +0530

    Added comments, show databases, use and show tables commands

--- Commands.sql
+++ Commands.sql
@@ -1,1 +1,24 @@
+--Comments in MySql
+--   Single Line Comments
+--       #   This comment continues to the end of line
+--       --  This comment continues to the end of line
+--       
+--   Inline Comments
+--       SELECT * FROM /* This is a inline comment*/ students;
+--
+--   Multi Line comments
+--   /* multiple line comments  */       
+--
 
+
+-- ----------------------------------------------------------------
+
+
+-- Show Databases 
+SHOW DATABASES;
+
+-- Select a database to work with
+USE database_name;
+
+-- Show tables in a Database
+SHOW TABLES;
