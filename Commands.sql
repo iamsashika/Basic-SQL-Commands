c4dce052e235af60f3cdfb5ed7e893cde0733b4e
Sashika Chandrasena <78399078+iamsashika@users.noreply.github.com>
2022-12-12 13:58:36 +0530

    Added table related commands

--- Commands.sql
+++ Commands.sql
@@ -31,4 +31,31 @@
 -- Rename Database
     #No Commands
 
+
+
+/*-- ----------------------------------------------------------------
+                    Table Related Commands
+--------------------------------------------------------------------*/
+
+-- Show Tables
+    SHOW TABLES;
+
+-- Create Table - Simple Table with no constraints
+    CREATE TABLE table_name (
+        attribute_name_1 INT,
+        attribute_name_2 VARCHAR,
+        attribute_name_3 DATE,
+        attribute_name_4 VARCHAR        
+    );
+
+
+-- Create Table - With Primary Key constraints 
+    CREATE TABLE table_name (
+        attribute_name_1 INT,
+        attribute_name_2 VARCHAR,
+        attribute_name_3 DATE,
+        attribute_name_4 VARCHAR        
+    );
+
+
 /*-------------------------------------------------------------------*/
