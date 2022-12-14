d767ef0bf6498ac3cc4dcee10f8cc45534bb3bdf
Sashika Chandrasena <78399078+iamsashika@users.noreply.github.com>
2022-12-14 13:11:48 +0530

    formatted

--- Commands.sql
+++ Commands.sql
@@ -42,91 +42,95 @@
 -- Show Tables
     SHOW TABLES;
 
--- Create Table - Simple Table with NO constraints
-    CREATE TABLE table_name (
-        attribute_name_1 INT,
-        attribute_name_2 VARCHAR,
-        attribute_name_3 DATE,
-        attribute_name_4 VARCHAR        
-    );
+    /*----------------------------------
+                CREATE TABLE 
+    -----------------------------------*/
 
-    /* Database Constraints
-        PRIMARY KEY
-        FOREIGN KEY
-        UNIQUE KEY
-        CHECK
-        NOT NULL
-        DEFAULT
-    */
+    -- Create Table - Simple Table with NO constraints
+        CREATE TABLE table_name (
+            attribute_name_1 INT,
+            attribute_name_2 VARCHAR,
+            attribute_name_3 DATE,
+            attribute_name_4 VARCHAR        
+        );
 
--- Create Table - With PRIMARY KEY constraints  # Method 1
-    CREATE TABLE table_name (
-        attribute_name_1 INT PRIMARY KEY,
-        attribute_name_2 VARCHAR,
-        attribute_name_3 DATE,
-        attribute_name_4 VARCHAR        
-    );
+        /* Database Constraints
+            PRIMARY KEY
+            FOREIGN KEY
+            UNIQUE KEY
+            CHECK
+            NOT NULL
+            DEFAULT
+        */
 
--- Create Table - With PRIMARY KEY constraints  # Method 2
-    CREATE TABLE table_name (
-        attribute_name_1 INT,
-        attribute_name_2 VARCHAR,
-        attribute_name_3 DATE,
-        attribute_name_4 VARCHAR,
-        PRIMARY KEY(attribute_name_1)        
-    );
+    -- Create Table - With PRIMARY KEY constraints  # Method 1
+        CREATE TABLE table_name (
+            attribute_name_1 INT PRIMARY KEY,
+            attribute_name_2 VARCHAR,
+            attribute_name_3 DATE,
+            attribute_name_4 VARCHAR        
+        );
 
--- Create Table - With Composite PRIMARY KEY constraints 
-    CREATE TABLE table_name (
-        attribute_name_1 INT,
-        attribute_name_2 VARCHAR,
-        attribute_name_3 DATE,
-        attribute_name_4 VARCHAR,
-        PRIMARY KEY(attribute_name_2, attribute_name_3)        
-    );
+    -- Create Table - With PRIMARY KEY constraints  # Method 2
+        CREATE TABLE table_name (
+            attribute_name_1 INT,
+            attribute_name_2 VARCHAR,
+            attribute_name_3 DATE,
+            attribute_name_4 VARCHAR,
+            PRIMARY KEY(attribute_name_1)        
+        );
 
--- Create Table - With FOREIGN KEY constraints 
-    CREATE TABLE table_1 (
-        tb_1_pk INT,
-        attribute_name_2 VARCHAR,
-        attribute_name_3 DATE,
-        attribute_name_4 VARCHAR,
-        tb_2_pk VARCHAR,
-        PRIMARY KEY(tb_1_pk), 
-        FOREIGN KEY(tb_2_pk)  REFERENCES table_2(tb_2_pk)    
-    );
+    -- Create Table - With Composite PRIMARY KEY constraints 
+        CREATE TABLE table_name (
+            attribute_name_1 INT,
+            attribute_name_2 VARCHAR,
+            attribute_name_3 DATE,
+            attribute_name_4 VARCHAR,
+            PRIMARY KEY(attribute_name_2, attribute_name_3)        
+        );
 
--- Create Table - With UNIQUE constraints 
-    CREATE TABLE table_1 (
-        attribute_name_1 INT UNIQUE,
-        attribute_name_2 VARCHAR,
-        attribute_name_3 DATE,
-        attribute_name_4 VARCHAR,
-    );
+    -- Create Table - With FOREIGN KEY constraints 
+        CREATE TABLE table_1 (
+            tb_1_pk INT,
+            attribute_name_2 VARCHAR,
+            attribute_name_3 DATE,
+            attribute_name_4 VARCHAR,
+            tb_2_pk VARCHAR,
+            PRIMARY KEY(tb_1_pk), 
+            FOREIGN KEY(tb_2_pk)  REFERENCES table_2(tb_2_pk)    
+        );
 
--- Create Table - With CHECK constraints 
-    CREATE TABLE table_1 (
-        attribute_name_1 INT CHECK (attribute_name_1 > 50),
-        attribute_name_2 VARCHAR,
-        attribute_name_3 DATE,
-        attribute_name_4 VARCHAR,
-    );
-  
--- Create Table - With NOT NULL constraints 
-    CREATE TABLE table_1 (
-        attribute_name_1 INT,
-        attribute_name_2 VARCHAR NOT NULL,
-        attribute_name_3 DATE,
-        attribute_name_4 VARCHAR,
-    );
+    -- Create Table - With UNIQUE constraints 
+        CREATE TABLE table_1 (
+            attribute_name_1 INT UNIQUE,
+            attribute_name_2 VARCHAR,
+            attribute_name_3 DATE,
+            attribute_name_4 VARCHAR,
+        );
 
--- Create Table - With DEFAULT constraints 
-    CREATE TABLE table_1 (
-        attribute_name_1 INT,
-        attribute_name_2 VARCHAR DEFAULT default,
-        attribute_name_3 DATE,
-        attribute_name_4 VARCHAR,
-    );
+    -- Create Table - With CHECK constraints 
+        CREATE TABLE table_1 (
+            attribute_name_1 INT CHECK (attribute_name_1 > 50),
+            attribute_name_2 VARCHAR,
+            attribute_name_3 DATE,
+            attribute_name_4 VARCHAR,
+        );
+    
+    -- Create Table - With NOT NULL constraints 
+        CREATE TABLE table_1 (
+            attribute_name_1 INT,
+            attribute_name_2 VARCHAR NOT NULL,
+            attribute_name_3 DATE,
+            attribute_name_4 VARCHAR,
+        );
+
+    -- Create Table - With DEFAULT constraints 
+        CREATE TABLE table_1 (
+            attribute_name_1 INT,
+            attribute_name_2 VARCHAR DEFAULT default,
+            attribute_name_3 DATE,
+            attribute_name_4 VARCHAR,
+        );
     
 -- Rename Table
 
